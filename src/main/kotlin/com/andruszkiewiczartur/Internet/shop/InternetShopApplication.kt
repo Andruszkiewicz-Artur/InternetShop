dd214c4e7ae0dba7074c105b03fe081315b42940
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2024-12-15 20:39:47 +0100

    first commit

---
+++ src/main/kotlin/com/andruszkiewiczartur/Internet/shop/InternetShopApplication.kt
@@ -0,0 +1,11 @@
+package com.andruszkiewiczartur.Internet.shop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InternetShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<InternetShopApplication>(*args)
+}
